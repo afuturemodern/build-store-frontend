5ba85a160a3098555b441234134968c080abde77
Chibu Ichiban <the1kidconnect@gmail.com>
2024-08-12 09:59:42 -0700

    formatted the getting started page to max-height and center the splash section

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -4,27 +4,34 @@ import React from "react";
 
 function GettingStarted() {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between content-center items-center">
       <Splash />
       <section className={cn("md:w-1/2 p-12")}>
-        <h1 className="text-5xl font-bold py-2">Let's get started.</h1>
-        <h2 className="text-md font-bold py-2">Tell us about your project.</h2>
         <form>
           <div className="flex">
-            <section>
-              <label className="text-xs">First Name</label>
+            <section className="flex flex-col py-2">
+              <label className="text-xs flex-1 box-border">First Name</label>
               <input className="border" type="text"></input>
             </section>
-            <section>
+            <section className="flex flex-col py-2 pl-4">
               <label className="text-xs">Last Name</label>
               <input className="border" type="text"></input>
             </section>
           </div>
-          <section>Work Email</section>
-          <section>Phone</section>
-          <section>Project Description</section>
-          <section>
-            <button>Submit</button>
+          <section className="flex flex-col py-2">
+            <label className="text-xs">Work Email</label>
+            <input className="border pl-4" type="text"></input>
+          </section>
+          <section className="flex flex-col py-2">
+            <label className="text-xs">Phone</label>
+            <input className="border" type="text"></input>
+          </section>
+          <section className="flex flex-col py-2">
+            <label className="text-xs">Project Description</label>
+            <input className="border" type="text"></input>
+          </section>
+          <section className="py-2">
+            <button className="bg-black text-white px-8 py-4">Submit</button>
           </section>
         </form>
       </section>
--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,10 +1,17 @@
-import { cn } from '@/utils/cn'
-import React from 'react'
+import { cn } from "@/utils/cn";
+import React from "react";
 
 function Splash() {
   return (
-    <div className={cn("bg-red-500 w-1/2","hidden md:flex")}>Splash</div>
-  )
+    <div className={cn("bg-red-500 w-1/2 h-lvh", "hidden md:flex flex-col content-center justify-center items-center")}>
+      <h1 className="w-1/2 text-5xl font-bold py-2">$BUILD with the best and brightest.</h1>
+      <h2 className="w-1/2 text-md py-2">
+        Fill out this form to access future modern's invite-only talent pool of
+        Fortune 500-level engineers, scientists, mathematicians, technologists,
+        designers, artists, business administrators, and professionals.
+      </h2>
+    </div>
+  );
 }
 
-export default Splash
+export default Splash;
