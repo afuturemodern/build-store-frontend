d9a49741c60e506a349f5e8242a525d7ddf6db06
Chibu Ichiban <the1kidconnect@gmail.com>
2024-08-18 16:57:54 -0700

    left-aligned GettingStarted splash logo and text

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,30 +1,42 @@
 import { cn } from "@/utils/cn";
 import React from "react";
 
+// function logo() {
+//   return (
+//     <a href="/" className={cn("")}>
+//           <img className="h-16 aspect-square" src="logo.png" alt="Logo" />
+//         </a>
+//   );
+// }
+
 function Splash() {
   return (
-    <div className={cn("lg:flex lg:w-1/2 bg-[#BBDCE3]")}>
-      <a href="/" className={cn("inline-block p-8")}>
+    <div
+      className={cn(
+        "h-lvh flex flex-col justify-between items-start",
+        "px-8",
+        "bg-[#BBDCE3]",
+        "lg:flex",
+        "lg:w-1/2",
+        "lg:p-8",
+        "lg:pl-48"
+      )}
+    >
+      <a href="/" className={cn("")}>
         <img className="h-16 aspect-square" src="logo.png" alt="Logo" />
       </a>
-      <div
-        className={cn(
-          "hidden",
-          "h-lvh flex flex-col justify-center items-start px-8 box-border"
-        )}
-      >
-        <div className="flex flex-col gap-8 max-w-lg text-white text-lg">
-          <h1 className="leading-snug text-5xl text-white font-extrabold block box-border">
-            $BUILD with the best and brightest.
-          </h1>
-          <h2 className="box-border block isolate">
-            Fill out this form to hire future modern's invite-only talent pool
-            of Fortune 500-level engineers, scientists, mathematicians,
-            technologists, designers, artists, business administrators, and
-            credentialed professionals.
-          </h2>
-        </div>
+      <div className="flex flex-col gap-8 max-w-lg text-white text-lg">
+        <h1 className="leading-snug text-5xl text-white font-extrabold block box-border">
+          $BUILD with the best and brightest.
+        </h1>
+        <h2 className="box-border block isolate">
+          Fill out this form to hire future modern's invite-only talent pool of
+          Fortune 500-level engineers, scientists, mathematicians,
+          technologists, designers, artists, business administrators, and
+          credentialed professionals.
+        </h2>
       </div>
+      <footer className="h-16"></footer>
     </div>
   );
 }
