7c05741d4bde4a1518b6521c986557db461a024c
Chibu Ichiban <the1kidconnect@gmail.com>
2024-08-18 08:30:16 -0700

    simplified css

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -4,39 +4,45 @@ import React from "react";
 
 function GettingStarted() {
   return (
-    <div className="flex justify-start items-stretch flex-wrap">
+    <div className={cn(
+    "flex flex-col lg:flex-row", 
+    "justify-start items-stretch"
+    )}>
       <Splash />
-      <div className={cn("flex-[0_0_50%]")}>
-        <div className="flex flex-col items-center">
-          <form className="h-lvh flex flex-col justify-center py-8">
-            <div className="flex">
-              <section className="flex flex-col py-2">
-                <label className="text-xs flex-1 box-border">First Name</label>
-                <input className="border" type="text"></input>
-              </section>
-              <section className="flex flex-col py-2 pl-4">
-                <label className="text-xs">Last Name</label>
-                <input className="border" type="text"></input>
-              </section>
-            </div>
-            <section className="flex flex-col py-2">
-              <label className="text-xs">Work Email</label>
-              <input className="border pl-4" type="text"></input>
-            </section>
-            <section className="flex flex-col py-2">
-              <label className="text-xs">Phone</label>
-              <input className="border" type="text"></input>
-            </section>
-            <section className="flex flex-col py-2">
-              <label className="text-xs">Project Description</label>
-              <input className="border" type="text"></input>
-            </section>
-            <section className="py-2">
-              <button className="bg-black text-white px-8 py-4">Submit</button>
-            </section>
-          </form>
+      <form className={cn(
+        "flex flex-col gap-4", 
+        "justify-center",
+        "content-center",
+        "w-1/2",
+        "lg:px-24"
+        )}>
+        <h1>Sign up to $BUILD your team.</h1>
+        <div className="flex flex-col md:flex-row">
+          <section className="flex flex-col">
+            <label className="text-xs flex-1 box-border">First Name</label>
+            <input className="border" type="text"></input>
+          </section>
+          <section className="flex flex-col">
+            <label className="text-xs">Last Name</label>
+            <input className="border" type="text"></input>
+          </section>
         </div>
-      </div>
+        <section className="flex flex-col">
+          <label className="text-xs">Work Email</label>
+          <input className="border" type="text"></input>
+        </section>
+        <section className="flex flex-col">
+          <label className="text-xs">Phone</label>
+          <input className="border" type="text"></input>
+        </section>
+        <section className="flex flex-col">
+          <label className="text-xs">Project Description</label>
+          <input className="border" type="text"></input>
+        </section>
+        <section className="">
+          <button className="bg-black text-white">Submit</button>
+        </section>
+      </form>
     </div>
   );
 }
--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -3,26 +3,28 @@ import React from "react";
 
 function Splash() {
   return (
-    <div className={cn("flex-[0_0_50%] bg-red-500")}>
-      <div className="h-lvh flex flex-col pl-[25%] pt-8 pb-24">
-        <a href="/" className={cn("inline-block")}>
-          <img className="h-16" src="logo.png" alt="Logo" />
-        </a>
-        <div className="h-lvh flex flex-col justify-center items-start box-border">
-          <div className="max-w-lg text-white text-lg">
-            <h1 className="mt-0 mb-4 text-5xl text-white font-extrabold block box-border">
-              $BUILD with the best and brightest.
-            </h1>
-            <h2 className="box-border block isolate">  
-              Fill out this form to hire future modern's invite-only talent pool
-              of Fortune 500-level engineers, scientists, mathematicians,
-              technologists, designers, artists, business administrators, and
-              credentialed professionals.
-            </h2>
-          </div>
+    <div className={cn("lg:flex lg:w-1/2 bg-[#BBDCE3]")}>
+      <a href="/" className={cn("inline-block p-8")}>
+        <img className="h-16 aspect-square" src="logo.png" alt="Logo" />
+      </a>
+      <div
+        className={cn(
+          "hidden",
+          "h-lvh flex flex-col justify-center items-start px-8 box-border"
+        )}
+      >
+        <div className="flex flex-col gap-8 max-w-lg text-white text-lg">
+          <h1 className="leading-snug text-5xl text-white font-extrabold block box-border">
+            $BUILD with the best and brightest.
+          </h1>
+          <h2 className="box-border block isolate">
+            Fill out this form to hire future modern's invite-only talent pool
+            of Fortune 500-level engineers, scientists, mathematicians,
+            technologists, designers, artists, business administrators, and
+            credentialed professionals.
+          </h2>
         </div>
       </div>
-      
     </div>
   );
 }
